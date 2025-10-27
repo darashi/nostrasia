db68dad4598ef05b9f70ae01e1fc58f2ea234c61
Yoji Shidara <dara@shidara.net>
2025-10-27 23:53:30 +0900

    Update relay configuration

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,10 @@
 export const searchRelays = [
 	'wss://search.nos.today',
 	'wss://relay.nostr.band',
-	'wss://relay.noswhere.com',
 ];
 
 export const profileRelays = [
 	'wss://nos.lol',
-	'wss://yabu.me',
+	'wss://directory.yabu.me',
 	'wss://relay.damus.io',
 ];
