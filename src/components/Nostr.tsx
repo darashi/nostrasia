9d36867e9de05fb037e4d7f21d78d81f922b4ff1
Yoji Shidara <dara@shidara.net>
2025-11-20 21:11:29 +0900

    Fade in/out

--- src/components/Nostr.tsx
+++ src/components/Nostr.tsx
@@ -187,7 +187,9 @@ export default function NostrView() {
 	return (
 		<div className="relative flex h-full flex-col">
 			<div ref={noteContainerRef} className="flex-1">
-				<Note event={event} profiles={profiles} currentTime={currentTime} />
+				<div key={event.id} className="animate-fade-in-out h-full">
+					<Note event={event} profiles={profiles} currentTime={currentTime} />
+				</div>
 			</div>
 			{events.length > 0 ? (
 				<div
@@ -196,7 +198,7 @@ export default function NostrView() {
 						indicatorHidden ? 'translate-y-full opacity-0' : 'opacity-100'
 					}`}
 				>
-					<div className="flex items-center justify-center gap-3 text-base-content/80">
+					<div className="flex items-center justify-center gap-3 text-base-content/80 animate-fade-in-out">
 						{events.map((item) => {
 							const isActive = item.id === event.id;
 							return (
